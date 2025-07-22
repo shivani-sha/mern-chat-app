9a4566da16a6d59827032023e6ca871351cfc544
anmol.chouhan <anmol.chouhan@innoviti.com>
2025-07-22 20:48:07 +0530

    gitignore - removed build

--- backend/server.js
+++ backend/server.js
@@ -43,19 +43,18 @@ app.use('/api/message', messageRoutes);
 
 //---------------------Deployment--------------------
 
-const __dirname1 = path.resolve();
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.use(express.static(path.join(__dirname1, 'frontend', 'build')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"));
-  })
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is Running Successfully");
-
   });
 }
 //----------------------Deployment--------------------
